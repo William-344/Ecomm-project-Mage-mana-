34075aead0c873150de5aedb632a205c8a3d4129
William-344 <william.c.schall0@gmail.com>
2026-04-22 19:57:22 +0000

    trying to fix something

--- cart.js
+++ cart.js
@@ -2,9 +2,19 @@ function updateCartCount() {
     const cart = JSON.parse(localStorage.getItem("cart")) || [];
     const cartLink = document.querySelector(".nav-buttons a[href='cart.html']");
 
-    if (!cartLink) return;
+    if (cartLink) {
+        cartLink.textContent = `Cart (${cart.length})`;
+    }
+}
 
-    cartLink.textContent = `Cart (${cart.length})`;
+function addToCart(name, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({ name, price });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    updateCartCount();
 }
 
 document.addEventListener("DOMContentLoaded", updateCartCount);
