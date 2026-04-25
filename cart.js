48951a4df434e3e45dc0dd8426f8f9fe0f1d3c4c
William-344 <william.c.schall0@gmail.com>
2026-04-25 20:37:05 +0000

    I feel so dumb. Cart should be fixed

--- cart.js
+++ cart.js
@@ -1,3 +1,21 @@
+function addToCart(name, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({ name, price: Number(price) });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+
+    updateCartCount();
+}
+function updateCartCount() {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const cartLink = document.getElementById("cart-link");
+
+    if (cartLink) {
+        cartLink.textContent = `Cart (${cart.length})`;
+    }
+}
+
 function displayCart() {
     const container = document.getElementById("cartItems");
     const totalEl = document.getElementById("total");
